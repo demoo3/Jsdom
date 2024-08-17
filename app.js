e460e51d86b8a49389205521be804d3d5b9a12f5
demoo3 demo2 <gulayeminova56@gmail.com>
2024-08-17 19:24:25 +0000

    todo

--- app.js
+++ app.js
@@ -1,37 +1,24 @@
 "use strict";
 $(document).ready(function () {
-    $('.box').css({
-        "padding": "30px",
-        "margin-top": "80px",
-        "border-radius": "40px",
-        "background-color": "#804c24",
-        "width": "380px",
-        "height": "300px"
-    });
-    $('.textbox h1').css({
-        'font-size': '50px',
-        'color': '#ffff',
-        'font-weight': '700'
-    });
-    $('.minibox').css({
-        'display':'flex',
-        'align-items':'center',
-        'flex-direction':'column'
-    });
-    $('input').css({
-        "width": "300px",
-        'margin':'30px 0 30px',
-        'padding':'20px 0px 20px 20px',
-        'font-size': '20px',
-        "border-radius": "25px"
-    });
-    $('.btn').css({
-        "width": "100px",
-        'font-size': '20px',
-        "background-color": "#ffff",
-        "border-radius": "25px",
-        "padding": "8px 0",
-        'margin-top': "6px",
-        'cursor':'pointer'
+    
+    
+    
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let a = (a) => {
+            for (let i = 0; i < a; i++) {
+                $('.col-md-12').append(`<h2>${a}</h2>`);
+            }
+        }
+        a($('input').val())
     });
 });
+
+
+// $('#todo').click(function (e) {
+//   e.preventDefault();
+//   if (!$.trim($('#inputtodo').val()) == '') {
+//
+//   }
+//  });
