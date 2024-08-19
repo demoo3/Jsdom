9254f805ce025f112ed886aaeaadb9f9dbe74bed
demoo3 demo2 <gulayeminova56@gmail.com>
2024-08-19 21:58:20 +0000

    todo

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
 "use strict";
 $(document).ready(function () {
-
-   //$('#todo').click(function (e) {
-   //    e.preventDefault();
-   //    function myel(myclass = "") {
-   //        if (!$.trim($('#inputme').val()) == '') {
-   //            let vall = $('#inputtodo').val()
-   //                , newH2 = $('<h2> </h2>')
-   //                , newDiv = $('<div class="line"> </div>')
-   //                , bigDiv = $(`<div ${myclass} > </div>`)
-   //            $(newH2).append(vall)
-   //            $(newDiv).append(newH2)
-   //            $(bigDiv).append(newDiv)
-   //            return bigDiv
-   //        }
-   //        else {
-   //            return
-   //        }
-   //    }
-   //    $("#messagetodo").append(myel())
-   //    $("#messagetodo").append(myel('class="end"'))
-   //    $("#inputtodo").val("")
-   //    document.querySelector("#messagetodo").scrollTo(0, document.querySelector("#messagetodo").scrollHeight)
-   //    document.querySelector("#messagetodo").scrollTo(0, document.querySelector("#messagetodo").scrollHeight)
-   //});
-
+    
     $('.btn').click(function (e) {
         e.preventDefault();
-
-        let a = (a) => {
-            for (let i = 0; i < a; i++) {
-                $('.col-md-12').append(`<h2>${a}</h2>`);
+        if (!$.trim($('input').val()) == '') {
+            let a = b => {
+                for (let i = 0; i < b; i++) { $('.col-md-12').append(`<h2>${b}</h2>`); }
             }
+            let v = f => Number(f) ? a(f) : $('.col-md-12').append(`<h2>${f}</h2>`);
+            v($('input').val())
         }
-        a($('input').val())
-    });
+        $('input').val('');
+    })
 });
 
